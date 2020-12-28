bfb7363d6c32fc6c95e19bbba5b549d17585f443
Mikhail Polyakov <mpolyakov@ivi.ru>
2020-12-28 13:21:38 +0300

    vim: added colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
 require('plugins')
 -- recompile plugin settings on save
 vim.api.nvim_command('autocmd BufWritePost plugins.lua PackerCompile')
+
+require('theme')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,4 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   -- Packer can manage itself as an optional plugin
   use {'wbthomason/packer.nvim', opt = true}
+
+  -- black and white colorscheme
+  use 'Lokaltog/vim-monotone'
 end)
