6904498aa3511564f4c4c213cb3734cb2c68ed9b
Mikhail Polyakov <mpolyakov@ivi.ru>
2021-01-18 15:33:54 +0300

    vim: added ack.vim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -98,4 +98,24 @@ return require('packer').startup(function()
 	  'lyokha/vim-xkbswitch',
 	  config = 'vim.cmd[[let g:XkbSwitchEnabled = 1]]'
   }
+
+  -- search
+  use {
+	  'mileszs/ack.vim',
+	  config = function()
+		  vim.api.nvim_exec(
+		  [[
+		  if executable('ag')
+			  let g:ackprg = 'ag'
+		  endif
+		  let g:ackhighlight = 1
+		  nnoremap <LocalLeader>f :LAck! '\b<C-R>=expand("<cword>")<CR>\b'  <C-R>=ProjectRootGuess()<CR><CR>
+		  nnoremap <LocalLeader>s :LAck! '\b<C-R>=expand("<cword>")<CR>\b' <C-R>=ProjectRootGuess()<CR>
+		  nnoremap <Leader>f :LAck! '\b<C-R>=expand("<cword>")<CR>\b' <C-R>=expand('%:p:h')<CR><CR>
+		  nnoremap <Leader>s :LAck! '\b<C-R>=expand("<cword>")<CR>\b' <C-R>=expand('%:p:h')<CR>
+		  ]],
+		  false
+		  )
+	  end
+  }
   end)
