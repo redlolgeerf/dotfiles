6533873d3e8b661687ddadb63a974e85b594daa8
Mikhail Polyakov <m.polyakov@bestdoctor.ru>
2021-07-16 10:19:20 +0300

    nvim: update nvim-compe settings

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,24 +21,36 @@ return require('packer').startup(function()
   use 'SirVer/ultisnips'
   -- snippets themselves
   use 'honza/vim-snippets'
-  -- completion
-  use {
-	  "hrsh7th/nvim-compe",
-	  config = function()
-		  require'compe'.setup {
-			  enabled = true;
-			  debug = false;
-			  min_length = 1;
-			  preselect = 'disable';
-			  allow_prefix_unmatch = false;
-			  source = {
-				  path = true;
-				  buffer = true;
-				  vsnip = true;
-				  nvim_lsp = true;
-				  ultisnips = true;
-			  };
-		  }
+
+   --completion
+   use {
+	   "hrsh7th/nvim-compe",
+	   config = function()
+		   vim.o.completeopt = "menuone,noselect"
+		   require'compe'.setup {
+			   enabled = true;
+			   autocomplete = true;
+			   debug = false;
+			   min_length = 1;
+			   preselect = 'enable';
+			   throttle_time = 80;
+			   source_timeout = 200;
+			   incomplete_delay = 400;
+			   max_abbr_width = 100;
+			   max_kind_width = 100;
+			   max_menu_width = 100;
+			   documentation = true;
+
+			   source = {
+				   --path = true;
+				   buffer = true;
+				   --calc = true;
+				   nvim_lsp = true;
+				   --nvim_lua = true;
+				   --vsnip = true;
+				   --ultisnips = true;
+			   };
+		   }
 	  end
   }
 
