16ff435e59f9182a61974480c11369afc943e818
Mikhail Poliakov <mikhail.polyakov@tessian.com>
2022-02-10 10:30:20 +0000

    nvim: add pytrize

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -186,5 +186,14 @@ return require('packer').startup(function()
 	  end
   }
 
+  use { -- pytrize {{{
+	  'AckslD/nvim-pytrize.lua',
+	  -- uncomment if you want to lazy load
+	  -- cmd = {'Pytrize', 'PytrizeClear', 'PytrizeJump'},
+	  -- uncomment if you want to lazy load but not use the commands
+	  -- module = 'pytrize',
+	  config = 'require("pytrize").setup()',
+  } -- }}}
+
 
   end)
