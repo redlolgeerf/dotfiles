0695b6d5070392342b8d4e4d6e13ebabb6d72e74
Mikhail Poliakov <mikhail.polyakov@tessian.com>
2022-04-20 14:00:59 +0100

    nvim: enable treesitter

--- nvim/lua/treesitter_configs.lua
+++ nvim/lua/treesitter_configs.lua
@@ -1,13 +1,15 @@
 local treesitter = require('nvim-treesitter.configs')
 treesitter.setup {
-	ensure_installed = "maintained",
+	ensure_installed = "all",
+	ignore_install = { "phpdoc" },
 	highlight = {
 		enable = true,
 	},
 	indent = {
-		enable = false,
+		enable = true,
 	},
 }
 
-vim.api.nvim_command('set foldmethod=expr')
-vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+vim.wo.foldmethod="expr"
+vim.o.foldexpr="nvim_treesitter#foldexpr()"
+--vim.wo.foldenable=false -- can be enabled directly in opened file - using 'zi' - toogle fold
