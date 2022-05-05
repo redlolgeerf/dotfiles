76c9658b8f58b07151d4dddf2354421892f161f1
Mikhail Poliakov <mikhail.polyakov@tessian.com>
2022-05-05 12:29:09 +0100

    nvim: use wilder

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -205,6 +205,44 @@ return require('packer').startup(function()
   }
 
   use {'folke/tokyonight.nvim'}
+  use {
+  	'gelguy/wilder.nvim',
+  	config = function()
+	local wilder = require('wilder')
+	wilder.setup({
+		modes = {':', '/', '?'},
+		next_key = '<Tab>',
+		previous_key = '<S-Tab>',
+		accept_key = '<Down>',
+		reject_key = '<Up>',
+	})
+	wilder.set_option('pipeline', {
+		wilder.branch(
+			wilder.cmdline_pipeline({
+				fuzzy = 1,
+				set_pcre2_pattern = 1,
+			}),
+			wilder.python_search_pipeline({
+				pattern = 'fuzzy',
+			})
+		),
+	})
+
+	local highlighters = {
+		wilder.pcre2_highlighter(),
+		wilder.basic_highlighter(),
+	}
+
+	wilder.set_option('renderer', wilder.renderer_mux({
+		[':'] = wilder.popupmenu_renderer({
+			highlighter = highlighters,
+		}),
+		['/'] = wilder.wildmenu_renderer({
+			highlighter = highlighters,
+		}),
+	}))
+  	end,
+  }
 
 
-  end)
+end)
