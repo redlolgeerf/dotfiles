fd0f6cb4d165a1179663ad11b3f0a23230324752
Mikhail Poliakov <mikhail.polyakov@tessian.com>
2024-10-02 14:08:50 +0100

    nvim: added lsp commands to telescope

--- nvim/lua/telescope_configs.lua
+++ nvim/lua/telescope_configs.lua
@@ -18,5 +18,7 @@ vim.api.nvim_set_keymap('n', '<LocalLeader>l', '<cmd>Telescope loclist<CR>', { n
 vim.api.nvim_set_keymap('n', '<LocalLeader>t', '<cmd>Telescope lsp_document_symbols<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<LocalLeader>g', '<cmd>Telescope grep_string<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<LocalLeader>b', '<cmd>Telescope git_bcommits<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', { noremap = true, silent = true })
 
 require("telescope").load_extension("ui-select")
