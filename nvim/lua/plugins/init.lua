0c584b34b28f81b1f9e8e0016ae71a90c095ca9e
Mikhail Poliakov <mpoliakov@proofpoint.com>
2026-03-17 11:03:00 +0000

    nvim: added conform plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -163,4 +163,18 @@ return {
 
 	'folke/tokyonight.nvim',
 	'Exafunction/windsurf.vim',
+	{
+		'stevearc/conform.nvim',
+		opts = {
+			lua = { "stylua" },
+			python = { "ruff" },
+			-- You can customize some of the format options for the filetype (:help conform.format)
+			rust = { "rustfmt", lsp_format = "fallback" },
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	}
 }
